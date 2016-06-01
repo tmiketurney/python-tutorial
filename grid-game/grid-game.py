3f8bfc3e4ab38306be29e59d00a12678a6196a4f
T.Michael Turney <tturney@trellisware.com>
2016-05-31 17:46:42 -0700

    grid-game.py: add logic for Initialize()
     * involves new global variables for game control

--- grid-game/grid-game.py
+++ grid-game/grid-game.py
@@ -1,10 +1,42 @@
 
-score = 50
+from random import choice
+
+# Global Data
+#############
+
+turn = 1
+score = 0
 goalscore = 100
 
-def Initialize():
+# representation of board is list of lists
+board = [[0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0]]
+
+# Helper Functions
+##################
+
+def InitializeGrid(board):
+    #Initialize Grid by random value
+    for i in range(8):
+        for j in range(8):
+            board[i][j] = choice(['Q', 'R', 'S', 'T', 'U'])
+
+def Initialize(board):
     #Initialize game
-    print("Initializing")
+    #Initialize grid
+    InitializeGrid(board)
+    #Initialize score
+    global score
+    score = 0
+    #Initialize turn number
+    global turn
+    turn = 1
 
 def ContinueGame(current_score, goal_score = 100):
     #Return false if game should end, true if game is not over
@@ -17,8 +49,11 @@ def DoRound():
     #Perform one round of the game
     print("Doing one round")
 
+# main() starts here...
+#######################
+
 #Initialize game
-Initialize()
+Initialize(board)
 
 #While game not over
 while ContinueGame(score, goalscore):
