328db23186c82316d45a1d0808495404eaf3fd2b
T.Michael Turney <tturney@trellisware.com>
2016-06-02 07:48:18 -0700

    grid-game.py: add logic for ContinueGame()

--- grid-game/grid-game.py
+++ grid-game/grid-game.py
@@ -1,12 +1,17 @@
 
+score = 50
+goalscore = 100
+
 def Initialize():
     #Initialize game
     print("Initializing")
 
-def ContinueGame():
+def ContinueGame(current_score, goal_score = 100):
     #Return false if game should end, true if game is not over
-    print("Checking to see if we should continue")
-    return True
+    if (current_score >= goal_score):
+        return False
+    else:
+        return True
 
 def DoRound():
     #Perform one round of the game
@@ -16,6 +21,6 @@ def DoRound():
 Initialize()
 
 #While game not over
-while ContinueGame():
+while ContinueGame(score, goalscore):
     #Do a round of the game
     DoRound()
