742801d787b8cc93bbd3e51a657afe6406425a35
T.Michael Turney <tturney@trellisware.com>
2016-06-05 16:45:25 -0700

    monte-carlo.py: simple simulation 0.00 to 0.06
     * 10000 simulations
     * each simulation is 10 years
     * starting balance 1000, grow by 0.00 - 0.06 each year

---
+++ matplotlib/monte-carlo.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+#
+#  @file    matplot-play.py
+#
+#  COPYRIGHT:
+#
+#     Copyright: Tools Made Tough, 2016:
+#
+#     License:  GPL v2.1
+#
+#  @author   T.Michael Turney
+#  @date     5.June.2016
+#  @version  1.01
+#
+
+import random
+from matplotlib.pyplot import hist, show
+
+def ChangeInBalance(initial_balance):
+    rate = random.uniform(0.0, 0.06)
+    return initial_balance*rate
+
+number_years = 10
+number_sims = 10000
+final_balances = []
+
+for i in range(number_sims):
+    # set initial conditions
+    time=0
+    balance=1000
+
+    while (time < number_years):
+        # Increase balance and time
+        balance += ChangeInBalance(balance)
+        time += 1
+
+    # Store time and balance in lists
+    final_balances.append(balance)
+
+# Output the simulation results
+hist(final_balances, bins=20)
+show()
