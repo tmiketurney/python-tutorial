cb428d27c1eb57961339b518e5cedf9287c75ec9
T.Michael Turney <tturney@trellisware.com>
2016-06-07 17:04:13 -0700

    json-example.py: simple example of json string/object

---
+++ class-inheritance/json-example.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+#
+#  @file    json-example.py
+#
+#  COPYRIGHT:
+#
+#     Copyright: Tools Made Tough, 2016:
+#
+#     License:  GPL v2.1
+#
+#  @author   T.Michael Turney
+#  @date     7.June.2016
+#  @version  1.01
+#
+
+var family = [{
+    "name" : "Jason",
+    "age" : "24",
+    "gender" : "male"
+},
+{
+    "name" : "Kyle",
+    "age" : "21",
+    "gender" : "male"
+}];
